67ecff8a95b99f5437606f2198daf230e886cfb7
tianxiaqiao <1307145450@qq.com>
2019-01-24 21:36:44 +0800

    四

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
-// 点击搜索图标事件
-$("#search").on("click",function(){
+
+// 点击首行搜索图标事件
+$(".magnifier").on("click",function(){
     $(".arrow").css({
         right : 167
     })
-    $(".header-expanded").hide("slow");
-    $(".search").toggle("slow");   
+    $(".header-expanded").hide(1000);  
+    $(".search").toggle(2000);
+     
 })
+$(".import").on("focus",function(){
+    $(this).val("");
+})
+$(".import").on("blur",function(){
+    $(this).val()!="" || $(this).val("想找什么随便搜"); 
+})
+
+
 
 
 // 点击登录头像事件
@@ -13,6 +23,6 @@ $("#login").on("click",function(){
     $(".arrow").css({
         right : 443
     })
-    $(".search").hide("slow");
-    $(".header-expanded").toggle("slow");
+    $(".search").hide("1000");
+    $(".header-expanded").toggle(2000);
 })
